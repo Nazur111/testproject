7b180548337411ac05c09e582a188b77b05777f5
Nazur111 <135415714+Nazur111@users.noreply.github.com>
2025-07-20 14:35:56 +0300

    init commit

--- main.py
+++ main.py
@@ -1,1 +1,31 @@
-print("hello Git")
+import sqlite3
+
+conn = sqlite3.connect("students.db")
+cursor = conn.cursor()
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS students(
+id ITEGER PRIMARY KEY AUTOINCREMENT,
+name TEXT,
+age INTEGER,
+major TEXT
+)
+""")
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS courses(
+course_id ITEGER PRIMARY KEY AUTOINCREMENT,
+course_name TEXT
+
+)
+""")
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS student_courses(
+id ITEGER PRIMARY KEY AUTOINCREMENT,
+course_id INTENGER,
+student_id INTENGER
+FOREIGN KEY(course_id) REFERENCES courses(id),
+FOREIGN KEY(student_id) REFERENCES students(id)
+)
+""")
